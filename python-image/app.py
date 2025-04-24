57de43d551b74b94adf8ceb32c8bf35e31695d9d
Ashitosh Bachute <129257337+Ashitosh9922@users.noreply.github.com>
2025-04-24 20:34:04 +0530

    Update app.py

--- python-image/app.py
+++ python-image/app.py
@@ -1,1 +1,20 @@
-print("This is my first image.")
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "👋 Hello! Welcome to my Dockerized Flask App!"
+
+@app.route("/time")
+def time():
+    now = datetime.now()
+    return jsonify({"current_time": now.strftime("%Y-%m-%d %H:%M:%S")})
+
+@app.route("/status")
+def status():
+    return jsonify({"status": "Running", "message": "App is healthy!"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
